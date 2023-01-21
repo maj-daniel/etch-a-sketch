1182b6e1fec1322c6c9a116edf9e6d5aca0edb10
maj-daniel <daniel.soareslb@gmail.com>
2023-01-21 13:07:05 -0300

    Feat: Make squares change color on mouse hover
    Created function that add the class "hovered", which
    have a diferente color.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector(".container");
 console.log(container);
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 16; i++){//construct the grid with square divs
     for(let j = 0; j < 16; j++){
         const block = document.createElement('div');
         block.classList.add(`L${i+1}`, `C${j+1}`, "pixel");
         container.appendChild(block);
     }
+}
+
+const pixels = document.querySelectorAll(".pixel");
+console.log(pixels);
+
+pixels.forEach(pixel =>{
+    pixel.addEventListener('mouseover', changeDivColor);
+});
+
+function changeDivColor(e) {
+    e.target.classList.add("hovered");
 }
