2c3fc55214ed28760b7f9457ebae0103fd29a0d4
maj-daniel <daniel.soareslb@gmail.com>
2023-01-21 12:43:18 -0300

    Feat: Make a generated grid
    Created code to generate a grid of 16 x 16 squares

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 const container = document.querySelector(".container");
+console.log(container);
+
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
+        const block = document.createElement('div');
+        block.classList.add(`L${i+1}`, `C${j+1}`, "pixel");
+        container.appendChild(block);
+    }
+}
