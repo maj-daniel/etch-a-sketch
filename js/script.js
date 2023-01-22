1fc772e7a4120f7ede0061edf1ddbe2ba7c7cc3c
maj-daniel <daniel.soareslb@gmail.com>
2023-01-22 11:14:08 -0300

    Feat: Make btn to generate custom grid size

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 const container = document.querySelector(".container");
 console.log(container);
 
-for(let i = 0; i < 16; i++){//construct the grid with square divs
-    for(let j = 0; j < 16; j++){
-        const block = document.createElement('div');
-        block.classList.add(`L${i+1}`, `C${j+1}`, "pixel");
-        container.appendChild(block);
-    }
-}
-
 const pixels = document.querySelectorAll(".pixel");
 console.log(pixels);
 
+const gridBtn = document.querySelector("#grid-btn");
+
+gridBtn.addEventListener("click", createNewGrid);
+
 pixels.forEach(pixel =>{
-    pixel.addEventListener('mouseover', changeDivColor);
+    pixel.addEventListener('mouseover', changeDivColor);//check is mouse hovered every pixel in the container
 });
 
-function changeDivColor(e) {
+function changeDivColor(e) { //add class that has diferent color
     e.target.classList.add("hovered");
+}
+
+function createNewGrid() {
+    const elementsToRemove = document.querySelectorAll('.pixel');
+    elementsToRemove.forEach(element => element.remove());//Clean previous grid
+    let lineAndColumnNumber;
+    do{
+        lineAndColumnNumber = parseInt(prompt("Enter a grid size (2 - 100)"));
+    } while (lineAndColumnNumber > 100 || lineAndColumnNumber < 2 || !lineAndColumnNumber);
+
+    for(let i = 0; i < lineAndColumnNumber; i++){//construct the grid with square divs
+        for(let j = 0; j < lineAndColumnNumber; j++){
+            const block = document.createElement('div');
+            block.classList.add(`L${i+1}`, `C${j+1}`, "pixel");
+            container.appendChild(block);
+        }
+    }
 }
